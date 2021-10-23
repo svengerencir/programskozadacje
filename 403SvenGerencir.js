9cc531abc717caeef8578e0e1173112fe06409d7
svengerencir <svengerencir@hotmail.com>
2021-10-23 22:24:56 +0200

    JS-403
    pregled pravilno unesenih zagrada

---
+++ 403SvenGerencir.js
@@ -0,0 +1,30 @@
+function zagrade(s) {
+  let tester = "";
+  if (s[0] == ")" || s[0] == "]" || s[0] == "}") return false;
+  tester = tester.concat(s[0]);
+  for (let i = 1; i < s.length; i++) {
+    if (
+      (s[i] == ")" && tester[tester.length - 1] == "[") ||
+      (s[i] == ")" && tester[tester.length - 1] == "{") ||
+      (s[i] == "]" && tester[tester.length - 1] == "(") ||
+      (s[i] == "]" && tester[tester.length - 1] == "{") ||
+      (s[i] == "}" && tester[tester.length - 1] == "[") ||
+      (s[i] == "}" && tester[tester.length - 1] == "(")
+    ) {
+      return false;
+    }
+    if (
+      (s[i] == ")" && tester[tester.length - 1] == "(") ||
+      (s[i] == "]" && tester[tester.length - 1] == "[") ||
+      (s[i] == "}" && tester[tester.length - 1] == "{")
+    ) {
+      tester = tester.slice(0, tester.length - 1);
+    } else {
+      tester = tester.concat(s[i]);
+    }
+  }
+  return true;
+}
+console.log(zagrade("[()]()()"));
+console.log(zagrade("{[((()))]}"));
+console.log(zagrade("({)}"));
