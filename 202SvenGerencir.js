2c8560e67d7285051c73e106341a89de339b5f5f
svengerencir <svengerencir@hotmail.com>
2021-10-23 15:13:50 +0200

    JS-202
    maknuta varijabla

--- 202SvenGerencir.js
+++ 202SvenGerencir.js
@@ -1,5 +1,3 @@
-let stringy = "";
 for (let i=1;i<101;i++){
-    stringy= !(i%3)?(!(i%5)?"FizzBuzz":"Fizz"):!(i%5)?"Buzz":i;
-    console.log(stringy);
+    console.log(!(i%3)?(!(i%5)?"FizzBuzz":"Fizz"):!(i%5)?"Buzz":i);
 }
