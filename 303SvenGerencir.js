25db41d649af6c6d9acab811f7149c532f1ad500
svengerencir <svengerencir@hotmail.com>
2021-10-23 20:23:21 +0200

    JS-303

--- 303SvenGerencir.js
+++ 303SvenGerencir.js
@@ -2,7 +2,7 @@ function ackermann(m,n){
     return (m==0)?n+1:((n==0)?ackermann(m-1,1):ackermann(m-1,ackermann(m,n-1)));
 }
 
-onsole.log(ackermann(0, 5))
+console.log(ackermann(0, 5))
 console.log(ackermann(5, 0))
 console.log(ackermann(3, 6))
 console.log(ackermann(3, 3))
