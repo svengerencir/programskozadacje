27f1659f594caaae1b6cef728da738dce34b0d49
svengerencir <svengerencir@hotmail.com>
2021-10-25 05:58:31 +0200

    JS-302
    maknuta nepotrebna varijabla

--- 302SvenGerencir.js
+++ 302SvenGerencir.js
@@ -1,15 +1,13 @@
-function istiZnakovi(rijec){
-    let max=0;
-    for (let i=0;i<rijec.length-1;i++){
-        let slovo=rijec.charAt(i);
-        let brojac=0;
-        for (let j=0;j<rijec.length;j++)
-            if (slovo==rijec.charAt(j))
-                brojac++;
-        if (brojac>max) max=brojac;
-    }
-    return max;
+function istiZnakovi(rijec) {
+  let max = 0;
+  for (let i = 0; i < rijec.length - 1; i++) {
+    let brojac = 0;
+    for (let j = i; j < rijec.length; j++)
+      if (rijec.charAt(i) == rijec.charAt(j)) brojac++;
+    if (brojac > max) max = brojac;
+  }
+  return max;
 }
 
-console.log(istiZnakovi("baaaccd"))
-console.log(istiZnakovi("ba2dll"))
+console.log(istiZnakovi("baaaccd"));
+console.log(istiZnakovi("ba2dll"));
